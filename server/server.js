98e3ccc22f69860b692581c01e9b8b485c57dfd6
Chiboy Cristian <chiboycristiansibarani@gmail.com>
2026-07-20 18:12:10 +0700

    fix: error cors

--- server/server.js
+++ server/server.js
@@ -12,19 +12,13 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 // Middleware
-app.use(cors());
-// app.use(
-//   cors({
-//     origin: "*", // Bebaskan origin untuk sementara
-//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-//     // 👇 Wajib daftarkan header ngrok di sini beserta header standar lainnya
-//     allowedHeaders: [
-//       "Content-Type",
-//       "Authorization",
-//       "ngrok-skip-browser-warning",
-//     ],
-//   }),
-// );
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json());
 
 // Import Routes
